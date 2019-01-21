fe567f3ee9a72629b1c3e4bab7310aab718f6e1e
RHowe22 <robjhowe22@gmail.com>
2019-01-21 16:40:09 -0500

    finished dec 3 and added comments

--- Dec2.py
+++ Dec2.py
@@ -1,7 +1,9 @@
+# class to check  if a string contains the same pair or triple of a letter
 class multiple:
-    two = False
-    three = False
+    two = False # reflects if the class contains a duplicate of a char
+    three = False # reflects in the class contains 3 of the same chars
 
+    # method that checks if a given string contains and duplicates or triples
     def find_multiples(self, string):
         index = 0
         while index < (len(string)):
@@ -11,26 +13,32 @@ class multiple:
                 self.three = True
             index += 1
 
+    def reset(self):
+        self.two = False
+        self.three = False
 
 fName = "C:\\Users\\rob\\source\\Atom\\AOC2018\\day2input.txt"
 file = open(fName)
 data = file.readlines()
-
-twocount = 0
+# collect data from the text file
+twocount = 0 # count the total number of duplicates and triples
 threecount = 0
 temp = multiple()
-for lines in data:
+# creates a multiple object to check all the strings in the data file
+
+for lines in data:  #use the multiple object to counnt all the duplicates and triples
     temp.find_multiples(lines)
     if temp.two:
         twocount += 1
     if temp.three:
         threecount += 1
-    temp.two = False
-    temp.three = False
+    temp.reset()
 print("twos: " + str(twocount) + " threes: " + str(threecount))
+
+# compare all the string to find the pair that only differ by one char
 i = 0
 numdiff = 0
-while numdiff != 1:
+while numdiff != 1:  # all strings are checked until the strings at index i and j differ by only one char
     j = i + 1
     while j < len(data) and numdiff != 1:
         numdiff = 0
@@ -41,12 +49,14 @@ while numdiff != 1:
             index += 1
         j += 1
     i += 1
+
+#  produce the a string that is all the shared chars between the two strings from the last section
 index = 0
 string = ""
 while index < len(data[i-1]):
     if(data[i-1][index] == data[j-1][index]):
         string = string.__add__(data[i-1][index])
     index += 1
-print(data[i-1])
-print(data[j-1])
+
+# print the resulting string
 print(string)
--- Dec3.py
+++ Dec3.py
@@ -1,30 +1,41 @@
 grid = [[0 for x in range(1000)] for y in range(1000)]
+# crete a grid for all the spaces that could be occupied
 
 fName = "C:\\Users\\rob\\source\\Atom\\AOC2018\\day3input.txt"
 file = open(fName)
 data = file.readlines()
-conflict = 0
+# read in the data
+
+conflict = 0  # variable to count all conflicting indexes
 good = {}
 for x in range(len(data)):
     good[x] = True
-for lines in data:
-    id = int(lines[lines.find('#') + 1: lines.find('@')])
-    xoff = int(lines[lines.find('@') + 1: lines.find(',')])
-    yoff = int(lines[lines.find(',') + 1: lines.find(':')])
-    x = int(lines[lines.find(':') + 1: lines.find('x')])
-    y = int(lines[lines.find('x') + 1:])
+# create a list of all the ids and set true to say they have no conflicts
+
+for lines in data: # read in all the lines of data and collect information
+    id = int(lines[lines.find('#') + 1: lines.find('@')])  # get the id of the claim
+    xoff = int(lines[lines.find('@') + 1: lines.find(',')]) # get x-axis offset
+    x = int(lines[lines.find(':') + 1: lines.find('x')])   # get the width of the claim
+    y = int(lines[lines.find('x') + 1:])    # get the height of the claim
+    yoff = int(lines[lines.find(',') + 1: lines.find(':')]) # get the y-axis offset
     # gather the id of the claim its x and y offests and the height and width of the fabric
+
+    # fill in the grid with the claims and if their is a conflict add it to the
+    # conflict count and mark the ids of the conflicting claims as false
     for width in range(x):
         for height in range(y):
             if grid[xoff+width][yoff+height] == 0:
                 grid[xoff+width][yoff+height] = id
             elif grid[xoff+width][yoff+height] != -1:
-                print(id)
-                good.pop(id)
+                good[id-1] = False
                 if grid[xoff+width][yoff+height] in good:
-                    del good[grid[xoff+width][yoff+height]]
+                    good[(grid[xoff+width][yoff+height])-1] = False
                 grid[xoff+width][yoff+height] = -1
                 conflict += 1
 
 print(conflict)
-print(good)
+# print conflict count and the id of the claim with no conflict
+for x in range(len(good)):
+    if good[x]:
+        print(x+1)
+        break
